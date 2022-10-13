20053af9ad6e28328cc0dd3b462f2183975000c9
bluesols <105667383+bluesols@users.noreply.github.com>
2022-10-13 15:39:18 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> BlueSols.!
+          Edit <code>src/App.js</code> BlueSols..!
         </p>
         <a
           className="App-link"
